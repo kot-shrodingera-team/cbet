9d0c6aa017694b4c89031a8de9611179e7aa1ee2
kot-shrodingera <kot.shrodingera@gmail.com>
2020-11-24 15:14:50 +0600

    fix(getParameter): параметр 0 при маркете Draw No Bet

--- src/stake_info/getParameter.ts
+++ src/stake_info/getParameter.ts
@@ -4,15 +4,26 @@ const getParameter = (): number => {
   const betNameElement = window.frames[1].document.querySelector(
     '.betslip .odd span:nth-child(2)'
   );
+  const marketNameElement = window.frames[1].document.querySelector(
+    '.betslip .market-name'
+  );
   if (!betNameElement) {
     log('Не найдена роспись ставки', 'crimson');
     return -9999;
   }
+  if (!marketNameElement) {
+    log('Не найден маркет ставки', 'crimson');
+    return -9999;
+  }
   const betName = betNameElement.textContent.trim();
+  const market = marketNameElement.textContent.trim();
   const parameterRegex = /\(([+-]?\d+(?:\.\d+)?)\)/;
   const parameterMatch = betName.match(parameterRegex);
   if (parameterMatch) {
     return Number(parameterMatch[1]);
+  }
+  if (market === 'Draw no bet') {
+    return 0;
   }
   // Ставка Exact total goals 3+ === Over (2.5)
   if (betName === '3+') {
